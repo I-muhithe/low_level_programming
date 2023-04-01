8900d91b9414d454adc18f0f587ff32a55dd11e5
I-muhithe <ivykibanya@gmail.com>
2023-04-01 14:05:46 -0700

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,11 +9,11 @@
 */
 size_t list_len(const list_t *h)
 {
-   int count = 0;
-   while (h)
-   {
-       count++;
-       h = h->next;
-   }
-   return (count);
+int count = 0;
+while (h)
+{
+count++;
+h = h->next;
+}
+return (count);
 }
